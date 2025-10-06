c65d9c8189608bd4b61c062473c1d62e660733ff
xrrrrsst211 <2023002@miu.edu.mn>
2025-10-06 21:54:14 +0800

    Initial Dart project

---
+++ bin/hashtables.dart
@@ -0,0 +1,5 @@
+import 'package:hashtables/hashtables.dart' as hashtables;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hashtables.calculate()}!');
+}
---
+++ test/hashtables_test.dart
@@ -0,0 +1,8 @@
+import 'package:hashtables/hashtables.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
